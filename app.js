73540c3f5ef61de666b2bd4961a1ee4e1d7812a6
Eduardo Gonçalves <eduardouc388@gmail.com>
2025-08-04 21:03:52 -0300

    Update app.js

--- app.js
+++ app.js
@@ -1,1 +1,46 @@
-//O principal objetivo deste desafio é fortalecer suas habilidades em lógica de programação. Aqui você deverá desenvolver a lógica para resolver o problema.
+let amigos = [];
+
+function adicionarAmigo()
+{
+    let amigo = document.querySelector('input').value;
+
+    if(amigo == '')
+    {
+        alert('Por favor, insira um nome válido.');
+    }
+    else if(amigos.includes(amigo))
+    {
+        alert('Este amigo já foi adicionado à lista.');
+    }
+    else
+    {
+        amigos.push(amigo);
+        document.querySelector('input').value = '';
+        atualizarLista();
+    }
+}
+
+function atualizarLista()
+{
+    let lista = document.getElementById('listaAmigos');
+    lista.innerHTML = '';
+
+    for (let i = 0; i < amigos.length; i++) {
+        const item = document.createElement('li');
+        item.textContent = amigos[i];
+        lista.appendChild(item); 
+    }
+}
+
+function sortearAmigo()
+{
+    if(amigos.length === 0)
+    {
+        alert('A lista de amigos está vazia. Adicione amigos para sortear.');
+        return;
+    }
+
+    let indiceAleatorio = Math.floor(Math.random() * amigos.length);
+    let amigoSorteado = document.getElementById('resultado');
+    amigoSorteado.innerHTML = amigos[indiceAleatorio];
+}
